99754a6a04a27e0c87d526ef23eebffa196e6d62
1oneand1 <123364703+1oneand1@users.noreply.github.com>
2023-04-01 06:34:26 -0700

    Add files via upload
    Why is None ????

---
+++ string_reverse.py
@@ -0,0 +1,13 @@
+def string_reverse(text, reverse_text):
+    for i in text:
+        index = i
+        if index == [-1]:
+            reverse_text.append(index)
+        else:
+            continue
+
+        
+index = ' '
+reverse_text = []
+text = input().split()
+print(string_reverse(text, reverse_text))
